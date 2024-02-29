eecf8284c4093921f2d46e64f82fe0a89b365e28
alaeddine <agarraoui@sqli.com>
2024-03-01 00:44:54 +0100

    controllers tus and tis

--- 02-backend/spring-boot-ecommerce/src/test/java/com/justatech/ecommerce/controllers/ProductCategoryControllerTest.java
+++ 02-backend/spring-boot-ecommerce/src/test/java/com/justatech/ecommerce/controllers/ProductCategoryControllerTest.java
@@ -1,28 +1,47 @@
 package com.justatech.ecommerce.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.justatech.ecommerce.domain.ProductCategory;
 import com.justatech.ecommerce.services.ProductCategoryService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductCategoryControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
     @Mock
     private ProductCategoryService mockProductCategoryService;
 
     @InjectMocks
     private ProductCategoryController productCategoryController;
 
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(productCategoryController).build();
+    }
+
     @Test
-    public void should_return_all_categories() {
+    public void should_return_all_categories() throws Exception {
         //Given
         ProductCategory booksCategory = new ProductCategory(1L, "BOOKS");
         ProductCategory mugsCategory = new ProductCategory(2L, "MUGS");
@@ -31,9 +50,13 @@ class ProductCategoryControllerTest {
         Mockito.when(mockProductCategoryService.findAll()).thenReturn(expectedCategories);
 
         //When
-        List<ProductCategory> actualCategories = productCategoryController.getCategories();
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/categories")).andExpect(status().isOk()).andReturn();
 
         //Then
-        Assertions.assertIterableEquals(actualCategories, expectedCategories);
+        String content = mvcResult.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<ProductCategory> actualProductCategories = objectMapper.readValue(content, new TypeReference<List<ProductCategory>>() {
+        });
+        Assertions.assertIterableEquals(actualProductCategories, expectedCategories);
     }
 }
