c3202d87a96c2d7551088658b29bb71fbea3160c
alaeddine <agarraoui@sqli.com>
2024-03-27 00:22:16 +0100

    integration des templates de front + libraries & refresh db with products and categories

---
+++ 02-backend/spring-boot-ecommerce/src/main/resources/db/migration/V5__refresh_category_table_with_3_categories.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Categories
+-- -----------------------------------------------------
+INSERT INTO product_category(category_name) VALUES ('Coffee Mugs');
+INSERT INTO product_category(category_name) VALUES ('Mouse Pads');
+INSERT INTO product_category(category_name) VALUES ('Luggage Tags');
--- 03-frontend/ecommerce-angular-app/src/app/shared/product.ts
+++ 03-frontend/ecommerce-angular-app/src/app/shared/product.ts
@@ -1,3 +1,11 @@
 export class Product {
-    constructor(public id:number, public name:string, public description:string, public unitPrice:number, public imgUrl:string) {}
+    constructor(
+        public sku:string, 
+        public name:string, 
+        public description:string, 
+        public unitPrice:number, 
+        public imageUrl:string,
+        public unitsInStock:number,
+        public dateCreated:Date,
+        public lastUpdated:Date) {}
 }
